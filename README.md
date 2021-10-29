ea8c4969d567932bbc9a6458e4b00c166f76b8fb
Pepe Fagoaga <jose.fagoaga@smartprotection.com>
2021-10-29 09:49:04 +0200

    feat(kms): first version

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # kms-helper
-Tool to encrypt/decrypt using AWS KMS with ease
+Tool to encrypt/decrypt using AWS KMS with ease.
+
+## Requirements
+ - AWS CLI v2
+ - AWS profiles configured at `~/.aws/config` or `~/.aws/credentials`
+ - Configuration file named `kms-helper.config` with the following structure:
+```
+# AWS ACCOUNT KMS KEYS
+PRO_KMS_KEY=""
+STG_KMS_KEY=""
+DEV_KMS_KEY=""
+
+# AWS ACCOUNT PROFILES
+PRO_AWS_PROFILE=""
+STG_AWS_PROFILE=""
+DEV_AWS_PROFILE=""
+```
+
+## Usage
+- Encrypt
+`./kms-helper.sh <encrypt> <plaintext> <account>`
+- Decrypt
+`./kms-helper.sh <decrypt> <encrypted> <account>`
---
+++ kms-helper.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Wrapper to encrypt/decrypt with AWS KMS
+# Requirements:
+# - AWS CLI v2
+# - AWS profiles configured at ~/.aws/config or ~/.aws/credentials
+
+# VARIABLES
+OPERATION="${1}"
+SECRET="${2}"
+ENV="${3}"
+OUTPUT_FORMAT="text"
+AWS_REGION="eu-west-1"
+CONFIG_PATH="./kms-helper.config"
+
+usage() {
+    echo "Usage: ./kms-helper.sh <encrypt/decrypt> <plaintext/encrypted> <account>"
+    exit 1
+}
+
+loadAWSconfig() {
+    if ! source "${CONFIG_PATH}" 
+    then
+        echo "Failed to load configuration file"
+        exit 1
+    fi
+}
+
+encrypt() {
+    PLAINTEXT=$(echo -n "${SECRET}" | base64)
+    ENCRYPT=$(aws kms encrypt --key-id "${KEY}" --plaintext "${PLAINTEXT}" --output "${OUTPUT_FORMAT}" --region "${AWS_REGION}" --profile "${PROFILE}" --query "${QUERY}")
+    echo "${ENCRYPT}"
+}
+
+decrypt() {
+    DECRYPT=$(aws kms decrypt --key-id "${KEY}" --ciphertext-blob "${SECRET}" --output "${OUTPUT_FORMAT}" --region "${AWS_REGION}" --profile "${PROFILE}" --query "${QUERY}" | base64 -D)
+    echo "${DECRYPT}"
+}
+
+checkKMS() {
+    # Check AWS KMS key
+    if [[ "${ENV}" == "pro" ]]
+    then
+        KEY="${PRO_KMS_KEY}"
+        PROFILE="${PRO_AWS_PROFILE}"
+    elif [[ "${ENV}" == "stg" ]]
+    then
+        KEY="${STG_KMS_KEY}"
+        PROFILE="${STG_AWS_PROFILE}"
+    elif [[ "${ENV}" == "dev" ]]
+    then
+        KEY="${DEV_KMS_KEY}"
+        PROFILE="${DEV_AWS_PROFILE}"
+    else
+        usage
+    fi
+}
+
+checkSecret() {
+    # Check secret
+    if [[ "${SECRET}" == "" ]]
+    then
+        usage
+    fi
+}
+
+selectOperation() {
+    # Check operation
+    if [[ "${OPERATION}" == "encrypt" ]]
+    then
+        QUERY="CiphertextBlob"
+        encrypt
+    elif [[ "${OPERATION}" == "decrypt" ]]
+    then
+        QUERY="Plaintext"
+        decrypt
+    else
+        usage
+    fi
+}
+
+loadAWSconfig
+checkSecret
+checkKMS
+selectOperation
